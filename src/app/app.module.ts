0309bc8f1df6e3f8a743d43c055c609295bc84b1
udathasuku <chowdary.sukumar@gmail.com>
2019-12-04 14:08:59 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {TableModule} from 'primeng/table';
+import { GridNewComponent } from './grid-new/grid-new.component';
+import {DialogModule} from 'primeng/dialog';
+import { DetailsViewComponent } from './details-view/details-view.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridNewComponent,
+    DetailsViewComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    TableModule,
+    DialogModule,
     AppRoutingModule
   ],
   providers: [],
